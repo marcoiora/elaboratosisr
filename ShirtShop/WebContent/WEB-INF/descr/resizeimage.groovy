6a3d3a7411a9e8066efc8b78e1d8733f76153899
Marco Iora <marco.iora@gmail.com>
2011-12-02 18:30:44 +0100

    fixes #35

--- ShirtShop/WebContent/WEB-INF/descr/resizeimage.groovy
+++ ShirtShop/WebContent/WEB-INF/descr/resizeimage.groovy
@@ -2,11 +2,7 @@
 #output String thumbnail
 
 
-import com.webratio.rtx.RTXBLOBData;
-import com.webratio.rtx.RTXException;
-import com.webratio.rtx.blob.BLOBData;
-import com.webratio.rtx.blob.ExternalBLOBData;
-import com.webratio.rtx.blob.ByteArrayBLOBData;
+import com.webratio.rtx.*;
 import com.webratio.rtx.blob.*
 
 import org.apache.commons.lang.StringUtils;
@@ -18,41 +14,49 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.File;
 
-
 RTXBLOBData blobFile = new BLOBData(orig_image, rtx);
 
-// Conversione
 InputStream input = blobFile.openFileInputStream();
 try{
+
 	BufferedImage image = ImageIO.read(input);
-	String ext = StringUtils.substringAfterLast(imageName, ".");
-	def width = image.getWidth(null);
-	def height = image.getHeight(null);
+	String ext = StringUtils.substringAfterLast(orig_image, ".");
+	
+	def width = 200;
+	def height = (width*image.getHeight()/image.getWidth()).intValue();
+	println height.class;
 	// per fare uno scale, utilizzare: AffineTransform 
-	BufferedImage grayScaleImage = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);  
-	Graphics g = grayScaleImage.getGraphics();  
-	g.drawImage(image, 0, 0, null);  
+	BufferedImage resizedImage = new BufferedImage(width, (Integer) height, BufferedImage.TYPE_3BYTE_BGR);  
+	Graphics g = resizedImage.getGraphics();
+	g.drawImage(image, 0, 0, width, height, 0, 0, image.getWidth(), image.getHeight(), null, null);
 	g.dispose();
-	ByteArrayOutputStream os = new ByteArrayOutputStream(); 
+	
+	File originalFile = new File(orig_image); 
+	def imageName = originalFile.getName();
+	String name = StringUtils.substringBeforeLast(imageName, ".");
+	name = name + java.util.UUID.randomUUID();
+	
+	String uploadDir = rtx.getUploadDirectory();
+ 
+	File thumbFile = new File(uploadDir + File.separator + name + "." + ext);
+	
+	thumbFile.createNewFile();
+	FileOutputStream os = new FileOutputStream(thumbFile);
+	
 	try {
-		ImageIO.write(grayScaleImage, ext.toUpperCase(), os);
+		ImageIO.write(resizedImage, ext.toUpperCase(), os);
 	} catch (IOException e) {
-	// TODO Auto-generated catch block
 		e.printStackTrace();
 	}
 	
-	
-	File originalFile = new File(orig_image);  
-	def imageName = originalFile.getName();
-	RTXBLOBData thumb = new ByteArrayBLOBData(imageName, os.toByteArray());
+	RTXBLOBService fileService = rtx.getBLOBService();
+	RTXBLOBData thumb = new BLOBData(fileService.getRelativePath(thumbFile), rtx);
+
+	return ["thumbnail" : thumb ]
 
 } catch (IOException e) {
- 
   throw new RTXException(e);  
  
 } finally {
- 
   IOUtils.closeQuietly(input);
- 
 }
-return ["thumbnail" : thumb ]
