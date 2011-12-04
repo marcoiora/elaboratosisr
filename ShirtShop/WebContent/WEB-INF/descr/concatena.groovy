551dd4bafded368e843c9839096d7c665aaa7f3c
Marco Iora <marco.iora@gmail.com>
2011-12-04 18:18:40 +0100

    fixes #27

---
+++ ShirtShop/WebContent/WEB-INF/descr/concatena.groovy
@@ -0,0 +1,7 @@
+#input String in1,String in2
+#output String out
+
+out = in1+in2;
+println out;
+println out.class;
+return ["out": out];
---
+++ ShirtShop/WebContent/WEB-INF/descr/debug.groovy
@@ -0,0 +1,8 @@
+#input colore_oid, sesso_oid
+#output colore_oid, sesso_oid
+
+println colore_oid;
+println sesso_oid;
+println "--";
+
+return ["colore_oid" : colore_oid, "sesso_oid": sesso_oid]
