b2a2cca8fc3d3fb544b8bfa403e3dc4136200b41
Andrea Piccinelli <frasten@gmail.com>
2011-12-22 19:26:15 +0100

    Sistemato servizio PHP, ora con client SOAP funzionante.

--- servizioclient.php
+++ servizioclient.php
@@ -48,38 +48,44 @@ a, a:visited {color: #f60;font-weight: bold;}
 <h2>Le ultime offerte</h2>
 <?php
 
-$base_url = "http://localhost:8080/ShirtShop/Servizi/";
+ini_set("soap.wsdl_cache_enabled", "0");
+
+$client = new SoapClient("http://127.0.0.1:8080/ShirtShop/Servizi/wsdl.do");
 
 // Servizio 1
-$url = $base_url . "ListaProposte/getListaProposte.do";
 
-$xmldata = file_get_contents($url);
+try {
+    $xmldata = $client->getListaProposte();
+    $listaProposte = $xmldata->Template->Instance;
+}
+catch (SoapFault $e) {
+    var_dump($e);
+}
 
-$xmldoc = new SimpleXMLElement($xmldata);
-
-$listaProposte = $xmldoc->Root[0]->Template[0]->Instance;
-echo "";
 echo "<table>";
 echo "<thead><tr>";
 echo "<th>Nome</th>";
 echo "<th class='descrizione'>Descrizione</th>";
 echo "<th>Prezzo</th>";
 echo "</tr></thead><tbody>";
-$index = 0;
-while (($row = $listaProposte[$index]) !== NULL) {
-	echo "<tr>";
-	echo "<td>";
-	printf("<a href='%s'>%s</a>", $row->URL, $row->nome);
-	echo "</td>";
+foreach ($listaProposte as $row) {
+    echo "<tr>";
+    echo "<td>";
+    printf("<a href='%s'>%s</a>", $row->URL, $row->nome);
+    echo "</td>";
 
-	echo "<td>{$row->descrizione}</td>";
+    echo "<td>{$row->descrizione}</td>";
 
-	printf("<td>%s</td>", number_format((float)$row->prezzo) . " &euro;");
+    printf("<td>%s</td>", number_format((float)$row->prezzo) . " &euro;");
 
-	echo "</tr>";
-	$index++;
+    echo "</tr>";
 }
 echo "</tbody></table>";
+
+// Servizio 2
+
+$url = $base_url;
+
 
 ?>
 </body>
